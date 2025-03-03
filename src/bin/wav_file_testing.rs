777dc98bf216f35deb9e0add582b657f0094577a
Erik Fong <quantumerikfong@gmail.com>
2025-03-02 20:51:16 -0500

    Attempt to do brf transmit
    does not compile, and I don't know why

--- src/bin/wav_file_testing.rs
+++ src/bin/wav_file_testing.rs
@@ -6,7 +6,16 @@ use std::{
     marker::PhantomData,
     mem::transmute,
     path::PathBuf,
+    time::Duration,
 };
+
+use bladerf::{
+    BladeRF, BladeRf1, BladeRfAny, Channel, ComplexI12, ComplexI16, Direction, SyncConfig,
+    brf_cf32_to_ci16, brf_ci12_to_cf32, brf_ci16_to_cf32,
+    expansion_boards::{Xb200Filter, Xb200Path},
+};
+
+use bladerf::Error as BrfError;
 
 use bladerf_nbfm_transceiver::{
     MY_TAPS_44100_20, MY_TAPS_882000_11,
@@ -23,6 +32,10 @@ use num::complex::Complex32;
 fn cf32_to_u8(arr: &[Complex32]) -> &[u8] {
     let ptr = arr.as_ptr() as *const u8;
     unsafe { std::slice::from_raw_parts(ptr, size_of_val(arr)) }
+}
+
+fn my_brf_error(err: BrfError) -> Error {
+    Error::new(ErrorKind::Other, format!("Bladerf Error: {err}"))
 }
 
 #[derive(Debug, Parser)]
@@ -67,32 +80,75 @@ fn main() -> Result<()> {
         _p: PhantomData::<Transmitting>,
     };
 
-    let mut output_buffer = Vec::with_capacity(audio_samples.len() * 11 * 20);
+    // let mut output_buffer = Vec::with_capacity(audio_samples.len() * 11 * 20);
 
     println!("Processing");
 
     use std::time::Instant;
     let now = Instant::now();
 
-    let tx_process = transmitter.process(&audio_samples);
+    let mut tx_process = transmitter.process(&audio_samples);
 
-    output_buffer.extend(tx_process);
+    let device: BladeRf1 = BladeRfAny::open_first()
+        .map_err(my_brf_error)?
+        .try_into()
+        .map_err(my_brf_error)?;
+
+    device
+        .set_sample_rate(Channel::Tx0, 1411200)
+        .map_err(my_brf_error)?;
+    let sync_confg = SyncConfig::default();
+
+    let xb200 = device.get_xb200().map_err(my_brf_error)?;
+    xb200
+        .set_path(Direction::TX, Xb200Path::Mix)
+        .map_err(my_brf_error)?;
+    xb200
+        .set_filterbank(Direction::TX, Xb200Filter::MHz144)
+        .map_err(my_brf_error)?;
+
+    device
+        .set_frequency(Channel::Tx0, 147_000_000)
+        .map_err(my_brf_error)?;
+
+    let streamer = device
+        .tx_streamer::<ComplexI16>(&sync_confg)
+        .map_err(my_brf_error)?;
+
+    // output_buffer.extend(tx_process);
     let elapsed = now.elapsed();
     println!("Elapsed: {:.2?}", elapsed);
 
-    let mut output_file = File::create_buffered(args.output_file)?;
-
-    for iq_sample in output_buffer {
-        let sample_bytes: [u8; 8] = unsafe { transmute(iq_sample) };
-        output_file.write_all(&sample_bytes)?;
-        // let a = iq_sample.re;
-        // let b = iq_sample.im;
-        // output_file.write_all(a.to_le_bytes().as_slice())?;
-        // output_file.write_all(b.to_le_bytes().as_slice())?;
+    // let mut my_loop = true;
+    let mut iq_buf = [ComplexI16::ZERO; 8192];
+    streamer.enable().map_err(my_brf_error)?;
+    'outer: loop {
+        for iq_sample in iq_buf.iter_mut() {
+            if let Some(new_samp) = tx_process.next() {
+                *iq_sample = brf_cf32_to_ci16(new_samp);
+            } else {
+                break 'outer;
+            }
+        }
+        streamer
+            .write(&iq_buf, Duration::from_secs(1))
+            .map_err(my_brf_error)?;
     }
-    println!("Finishing");
 
-    output_file.flush()?;
+    streamer.disable().map_err(my_brf_error)?;
+    // let mut output_file = File::create_buffered(args.output_file)?;
+
+    // for iq_sample in output_buffer {
+    //     let sample_bytes: [u8; 8] = unsafe { transmute(iq_sample) };
+    //     output_file.write_all(&sample_bytes)?;
+    //     // let a = iq_sample.re;
+    //     // let b = iq_sample.im;
+    //     // output_file.write_all(a.to_le_bytes().as_slice())?;
+    //     // output_file.write_all(b.to_le_bytes().as_slice())?;
+    // }
+    // println!("Finishing");
+
+    // output_file.flush()?;
 
     // println!("Mod sample count: {}", modulated.len());
 
